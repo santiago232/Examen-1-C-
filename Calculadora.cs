b85148f05d90722eae14e8cfcd5402c67c0f9357
fazt <wilson@faztweb.com>
2024-03-02 10:59:27 -0600

    Examen terminado

---
+++ Calculadora.cs
@@ -0,0 +1,49 @@
+﻿using System;
+
+class Calculadora
+{
+    public double Suma(double num1, double num2)
+    {
+        return num1 + num2;
+    }
+
+    public double Resta(double num1, double num2)
+    {
+        return num1 - num2;
+    }
+
+    public double Multiplicacion(double num1, double num2)
+    {
+        return num1 * num2;
+    }
+
+    public double Division(double num1, double num2)
+    {
+        if (num2 == 0)
+        {
+            throw new ArgumentException("No se puede dividir por cero.");
+        }
+        return num1 / num2;
+    }
+}
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Calculadora calc = new Calculadora();
+
+        // Pruebas de los métodos
+        Console.WriteLine("Suma: " + calc.Suma(5, 3));
+        Console.WriteLine("Resta: " + calc.Resta(5, 3));
+        Console.WriteLine("Multiplicación: " + calc.Multiplicacion(5, 3));
+        try
+        {
+            Console.WriteLine("División: " + calc.Division(5, 3));
+        }
+        catch (ArgumentException e)
+        {
+            Console.WriteLine("Error: " + e.Message);
+        }
+    }
+}
---
+++ Numeros Impares y Pares.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.Write("Ingrese la cantidad de números que desea ingresar: ");
+        int cantidadNumeros = int.Parse(Console.ReadLine());
+
+        int[] numeros = new int[cantidadNumeros];
+        int pares = 0, impares = 0, sumaPares = 0, sumaImpares = 0;
+
+        for (int i = 0; i < cantidadNumeros; i++)
+        {
+            Console.Write($"Ingrese el número {i + 1}: ");
+            numeros[i] = int.Parse(Console.ReadLine());
+
+            if (numeros[i] % 2 == 0)
+            {
+                pares++;
+                sumaPares += numeros[i];
+            }
+            else
+            {
+                impares++;
+                sumaImpares += numeros[i];
+            }
+        }
+
+        Console.WriteLine($"Cantidad de números pares: {pares}");
+        Console.WriteLine($"Cantidad de números impares: {impares}");
+        Console.WriteLine($"Suma de números pares: {sumaPares}");
+        Console.WriteLine($"Suma de números impares: {sumaImpares}");
+    }
+}
